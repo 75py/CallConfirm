fec2fdb4910e803dc79cc8860c9fe40a37e6b86b
75py <dev.75py@gmail.com>
2017-07-22 15:40:16 +0900

    Use RxLifecycle and replace callbacks to Rx

--- app/src/test/java/com/nagopy/android/callconfirm/viewmodel/ConfirmViewModelTest.java
+++ app/src/test/java/com/nagopy/android/callconfirm/viewmodel/ConfirmViewModelTest.java
@@ -20,8 +20,6 @@ import android.view.View;
 
 import com.nagopy.android.callconfirm.helper.HookState;
 import com.nagopy.android.callconfirm.view.helper.Navigator;
-import com.nagopy.android.callconfirm.viewmodel.ConfirmViewModel.OnCancelListener;
-import com.nagopy.android.callconfirm.viewmodel.ConfirmViewModel.OnFinishListener;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -29,12 +27,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import io.reactivex.observers.TestObserver;
+
 import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.anyBoolean;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -47,15 +45,6 @@ public class ConfirmViewModelTest {
     Navigator navigator;
 
     @Mock
-    OnFinishListener onFinishListener;
-
-    @Mock
-    OnCancelListener onCancelListener;
-
-    @Mock
-    ConfirmViewModel.OnLongClickListener onLongClickListener;
-
-    @Mock
     View view;
 
     @InjectMocks
@@ -64,33 +53,6 @@ public class ConfirmViewModelTest {
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-    }
-
-    @Test
-    public void setOnFinishListener() throws Exception {
-        OnFinishListener l = mock(OnFinishListener.class);
-
-        viewModel.setOnFinishListener(l);
-
-        assertThat(viewModel.onFinishListener).isEqualTo(l);
-    }
-
-    @Test
-    public void setOnCancelListener() throws Exception {
-        OnCancelListener l = mock(OnCancelListener.class);
-
-        viewModel.setOnCancelListener(l);
-
-        assertThat(viewModel.onCancelListener).isEqualTo(l);
-    }
-
-    @Test
-    public void setOnLongClickListener() throws Exception {
-        ConfirmViewModel.OnLongClickListener l = mock(ConfirmViewModel.OnLongClickListener.class);
-
-        viewModel.setOnLongClickListener(l);
-
-        assertThat(viewModel.onLongClickListener).isEqualTo(l);
     }
 
     @Test(expected = RuntimeException.class)
@@ -117,51 +79,42 @@ public class ConfirmViewModelTest {
 
     @Test
     public void destroy() throws Exception {
-        viewModel.onFinishListener = null;
-        viewModel.onCancelListener = null;
-
-        viewModel.setOnFinishListener(mock(OnFinishListener.class));
-        viewModel.setOnCancelListener(mock(OnCancelListener.class));
-        assertThat(viewModel.onFinishListener).isNotNull();
-        assertThat(viewModel.onCancelListener).isNotNull();
-
         viewModel.destroy();
-
-        assertThat(viewModel.onFinishListener).isNull();
-        assertThat(viewModel.onCancelListener).isNull();
     }
 
     @Test
     public void onClickCall() throws Exception {
         doNothing().when(hookState).setHookEnabled(anyBoolean());
         doNothing().when(navigator).call(anyString());
-        doNothing().when(onFinishListener).onFinish();
         String pn = "1234567890";
         viewModel.setPhoneNumber(pn);
+        TestObserver<Void> test = viewModel.callObserver.test();
 
         viewModel.onClickCall.onClick(null);
 
         verify(hookState, times(1)).setHookEnabled(false);
         verify(navigator, times(1)).call(pn);
-        verify(onFinishListener, times(1)).onFinish();
+        test.assertComplete();
     }
 
     @Test
     public void onClickCancel() throws Exception {
-        doNothing().when(onCancelListener).onCancel();
+        TestObserver<Void> test = viewModel.cancelObserver.test();
 
         viewModel.onClickCancel.onClick(null);
 
-        verify(onCancelListener, times(1)).onCancel();
+        test.assertComplete();
     }
 
     @Test
     public void onLongClick() throws Exception {
-        doNothing().when(onLongClickListener).onLongClick(view);
+        TestObserver<View> test = viewModel.longClickObserver.test();
 
         viewModel.onLongClick.onLongClick(view);
 
-        verify(onLongClickListener, times(1)).onLongClick(view);
+        test.assertValue(view)
+                .assertNoErrors();
+
     }
 
 }
